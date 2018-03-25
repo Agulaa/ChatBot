041131b7113cc167216fdf15cab4384dc3b31b7d
Agulaa <agnieszkaxrusin@gmail.com>
2018-03-26 01:23:02 +0200

    Change main method

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Rasa_nlu import Rasa_NLU
 import warnings
 from functools import wraps
+import json
 
 def ignore_warnings(f):
     """Function to ignore warnings in main method"""
@@ -22,6 +23,9 @@ def main():
         print('User: {}'.format(message))
         respond = rasa_nlu.find_respond_from_bot(message)
         print('BOT: {}'.format(respond))
+        if rasa_nlu.parse_message(message)['intent']['name'] == 'goodbye':
+            return
+
 
 
 if __name__ == '__main__':
